2b89dd8213873f235a12d11197f2113d3b7419b6
amosenbacher <anton.mosenbacher@kentor.se>
2018-11-05 11:36:36 +0100

    Setting up metadata for swagger

--- src/main/java/guru/springfamework/configurations/SwaggerConfiguration.java
+++ src/main/java/guru/springfamework/configurations/SwaggerConfiguration.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 @EnableSwagger2
 @Configuration
@@ -19,6 +23,25 @@ public class SwaggerConfiguration {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .pathMapping("/");
+                .pathMapping("/")
+                .apiInfo(metaData()); //adding metadata
     }
+
+    private ApiInfo metaData() {
+        //Contact -> Swagger documentation service
+        Contact contact = new Contact("Anton M", "http://www.fakeurl.com", "anton.M@fake.com");
+
+        return new ApiInfo(
+                "Spring framework title",
+                "Spring framework description",
+                "version 1.0",
+                "Terms of service",
+                contact,
+                "License ",
+                "License url",
+                new ArrayList<>()
+        );
+
+    }
+
 }
